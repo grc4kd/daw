776265b418dd46ee23bb6592e8136fe59d963575
Griffin Crane <grc4kd@users.noreply.github.com>
2022-08-28 15:24:12 -0500

    Mocks are nice but not neccessary and slower.

--- TestsNUnit/AudioProjectTests.cs
+++ TestsNUnit/AudioProjectTests.cs
@@ -5,16 +5,14 @@ public class AudioProjectTests
     [Test]
     public void CreateProjectTest()
     {
-        using var mock = AutoMock.GetLoose();
-        var sut = mock.Create<AudioProject>();
+        var sut = new AudioProject();
         Assert.That(sut.Name, Is.EqualTo("defaultProject"));
     }
     
     [Test]
     public void ChangeProjectNameTest()
     {
-        using var mock = AutoMock.GetLoose();
-        var sut = mock.Create<AudioProject>();
+        var sut = new AudioProject();
         const string name = "project eagle"; 
         sut.Name = name;
         Assert.That(sut.Name, Is.EqualTo(name));
@@ -23,8 +21,7 @@ public class AudioProjectTests
     [Test]
     public void ChangeProjectWebsiteTest()
     {
-        using var mock = AutoMock.GetLoose();
-        var sut = mock.Create<AudioProject>();
+        var sut = new AudioProject();
         const string website = "https://www.kumc.edu/project-eagle.html";
         sut.Website = website;
         Assert.That(sut.Website, Is.EqualTo(website));
